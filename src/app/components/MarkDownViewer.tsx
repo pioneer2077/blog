7db645b66192a89673cf01c109fd5333f8167f55
pioneer2077 <134494401+pioneer2077@users.noreply.github.com>
2023-12-03 18:38:10 +0900

    feat: MarkDownViewer Syntax highlight

--- src/app/components/MarkDownViewer.tsx
+++ src/app/components/MarkDownViewer.tsx
@@ -1,13 +1,38 @@
+"use client";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { cb } from "react-syntax-highlighter/dist/esm/styles/prism";
 type Props = {
   content: string;
 };
 export default function MarkDownViewer({ content }: Props) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className={"prose lg:prose-xl max-w-none"}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { ref, children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...rest}
+              style={cb}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -15,20 +15,22 @@ export default async function PostPage({ params: { slug } }: Props) {
     await getPostData(slug);
 
   return (
-    <article>
+    <article className=" rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
+        className="w-full max-h-[500px]"
         src={`/images/posts/${path}.png`}
         alt={title}
         width={760}
         height={420}
       />
-      <section>
-        <div>
+      <section className=" flex flex-col p-4">
+        <div className=" flex items-center self-end text-sky-600">
           <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
         </div>
-        <p>{date.toString()}</p>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
         <MarkDownViewer content={content} />
       </section>
     </article>
